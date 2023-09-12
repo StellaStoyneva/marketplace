d5bfd0d64c72406f751b9bf93bb67f106a5c4e9a
Ivaylo Grahlyov <ivaylo.grahlyov@gmail.com>
2023-09-12 21:31:02 +0300

    Fix test

--- src/features/products/routes/add.products.route.test.ts
+++ src/features/products/routes/add.products.route.test.ts
@@ -1,38 +1,51 @@
 import { register } from '../../../extensions/zod';
 register();
+import { ObjectId } from 'mongodb';
 import { addProduct } from './add.product.route';
 
-describe('add Product route handler', function () {
+describe('add Product route handler', function() {
   let routeHandler: any;
   let mockFastify: any;
   let collection: any;
-  let insertOne: any;
+  let insertFn: any;
+  let userId: any;
+  let store: any;
 
-  beforeEach(function () {
+  beforeEach(function() {
+    insertFn = jest.fn();
+    collection = jest.fn(() => ({ insertOne: insertFn, insertMany: insertFn }))
     mockFastify = {
       post: (_path: any, _opts: any, handler: never) => {
         routeHandler = handler;
       },
       db: jest.fn(() => ({
         name: 'marketplace',
-        collection: jest.fn(() => ({ insertOne: jest.fn() })),
+        collection: collection,
       })),
     };
 
+    userId = new ObjectId();
+    store = new ObjectId();
+
     addProduct(mockFastify as never, undefined as never, () => null);
   });
-  it('works', function () {
+  it('works', function() {
+    const body = [
+      {
+        name: 'testName',
+        sku: 'testCode',
+        productCategories: [new ObjectId()],
+        productTypes: new ObjectId()
+      },
+    ]
     routeHandler({
-      body: [{ name: 'testName', sku: 'testCode' }],
-      user: { _id: 'testUserId', store: 'testStore' },
+      body,
+      user: { _id: userId, store },
     });
 
     expect(mockFastify.db).toBeCalled();
-    expect(mockFastify.db.collection).toBeCalled();
-    // expect(mockFastify.db.collection('products').insertOne.toBeCalledWith({
-    //   name: 'testName',
-    //   productCode: 'testCode',
-    // });
+    expect(collection).toBeCalled();
+    expect(insertFn.mock.calls[0][0]).toMatchObject(body)
   });
 });
 
