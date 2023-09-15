03dd63a6d2559fd1ec0fd27b5ccacd664d771975
stella.stoyneva <stella.stoyneva@gmail.com>
2023-09-15 19:48:37 +0300

    feat: add order transaction

--- src/features/orders/routes/add.order.route.ts
+++ src/features/orders/routes/add.order.route.ts
@@ -1,6 +1,6 @@
 import { FastifyPluginCallback, RawServerDefault } from 'fastify';
 import { ZodTypeProvider } from 'fastify-type-provider-zod';
-import { Collection, MongoError, ObjectId, ServerSession } from 'mongodb';
+import { MongoError, ObjectId, TransactionOptions } from 'mongodb';
 import { DeliveryStatusEnum, OrderItemLifeCycleEnum } from 'src/constants/enum';
 import { CollectionEnum } from 'src/db/enum/collection.enum';
 import { IUser } from 'src/plugins/authentication';
@@ -21,132 +21,197 @@ export const addOrder: FastifyPluginCallback<
       },
     },
     async function addOrderHandler(req: { body: any; user: IUser }) {
-      console.log(req.body);
-      let order;
+      const orderResult: any[] = [];
+      const currentDeliveryStatus = DeliveryStatusEnum.ordered;
+      const getOrderItem = (item: any) => {
+        const {
+          name,
+          sku,
+          image,
+          quantity,
+          singlePriceBeforeVAT,
+          singlePriceWithVAT,
+          finalPriceBeforeVAT,
+          finalPriceWithVAT,
+          daysForReturn,
+        } = item;
+
+        return {
+          name,
+          sku,
+          image,
+          quantity,
+          singlePriceBeforeVAT,
+          singlePriceWithVAT,
+          finalPriceBeforeVAT,
+          finalPriceWithVAT,
+          daysForReturn,
+          ...flags[currentDeliveryStatus as keyof typeof DeliveryStatusEnum],
+          status: OrderItemLifeCycleEnum.ordered,
+        };
+      };
       const session = fastify.dbClient.startSession();
+
       try {
-        session.startTransaction();
-        console.log('Session started');
+        const transactionOptions: TransactionOptions = {
+          readPreference: 'primary',
+          readConcern: { level: 'local' },
+          writeConcern: { w: 'majority' },
+        };
 
-        // 1. TODO create invoice
-        // 2. get customer details
-        const customer = req.user._id;
-        const userDetails = await fastify
-          .db(process.env.DB_NAME as string)
-          ?.collection(CollectionEnum.Users)
-          .findOne({ _id: new ObjectId(customer) }, { session });
-
-        console.log({ userDetails });
-
-        const { ordersToStore } = req.body; // transaction id => finalPriceWithVAT
-
-        if (!Array.isArray) {
-          throw new Error('Invalid input format - expected array');
-        }
-        if (Array.isArray(ordersToStore)) {
-          console.log(47);
-          const arr = ordersToStore.map((res) => {
-            // 3. update products in order items quantity
-            res.items.forEach(async (element: any) => {
-              const product = await fastify
-                .db(process.env.DB_NAME as string)
-                ?.collection(CollectionEnum.Products)
-                .findOne(
-                  {
-                    _id: new ObjectId(element._id),
-                  },
-                  { session }
-                );
-              console.log({ product });
-
-              if (!product) {
-                throw new Error(`Product with _id ${element._id} not found`);
-              }
-              if (element.quantity > product.availableQuantity) {
-                throw new Error(
-                  `Insufficient quantity of product ${element._id}. Max quantity is ${product.availableQuantity}`
-                );
-              }
-              await fastify
-                .db(process.env.DB_NAME as string)
-                ?.collection(CollectionEnum.Products)
-                .updateOne(
-                  {
-                    _id: new ObjectId(element._id),
-                  },
-                  { $inc: { availableQuantity: -element.quantity } },
-                  { session }
-                );
-            });
-            // 3. create order
-            const currentDeliveryStatus = DeliveryStatusEnum.ordered;
-            console.log({ currentDeliveryStatus });
-
-            return {
-              ...res.items.map((item: any) => ({
-                ...item,
-                ...flags[
-                  currentDeliveryStatus as keyof typeof DeliveryStatusEnum
-                ],
-                status: OrderItemLifeCycleEnum.ordered,
-              })),
-              customer,
-              customerContact: {
-                email: req.user.email,
-                phoneNumber: userDetails?.phoneNumber,
-              },
-              store: new ObjectId(res.store),
-              createdAt: new Date(),
-              createdBy: customer,
-              deliveryStatus: fastify.deliveryStatusEnum?.find(
-                (status: { _id: ObjectId; deliveryStatus: string }) =>
-                  status?.deliveryStatus === DeliveryStatusEnum.ordered
-              )._id,
-            };
-          });
-
-          console.log({ arr });
-
-          order = await fastify
+        await session.withTransaction(async () => {
+          // 1. get customer details
+          const customer = req.user._id;
+          const userDetails = await fastify
             .db(process.env.DB_NAME as string)
-            ?.collection(CollectionEnum.Orders)
-            .insertMany(arr, { session });
-          return order;
+            ?.collection(CollectionEnum.Users)
+            .findOne({ _id: new ObjectId(customer) }, { session });
 
-          // console.log({ order });
+          const { ordersToStore } = req.body; // transaction id => finalPriceWithVAT
 
-          //5. TODO complete payment - if paymentType === card/ paypal
-          //6. TODO emit event
-          // event handler -> TODO send confirmation email -> emit event
-          // event handler -> create invoice
-          //return order;
-        }
-        await session.commitTransaction();
+          if (!Array.isArray) {
+            throw new Error('Invalid input format - expected array');
+          }
+          if (Array.isArray(ordersToStore)) {
+            //2. check if products exist and there's enough quantity
+            const productsInOrder = ordersToStore.reduce(
+              (
+                products: { _id: ObjectId; item: any; store: string }[],
+                order: any
+              ) => {
+                const itemsArray = order.items.reduce(
+                  (
+                    acc: { _id: ObjectId; item: any; store: ObjectId }[],
+                    element: any
+                  ) => {
+                    acc.push({
+                      _id: new ObjectId(element._id),
+                      item: getOrderItem(element),
+                      store: order.store,
+                    });
+                    return acc;
+                  },
+                  []
+                );
 
-        console.log('Transaction successfully committed.');
+                itemsArray.forEach(
+                  (element: { _id: ObjectId; item: any; store: string }) => {
+                    products.push(element);
+                  }
+                );
+                return products;
+              },
+              []
+            );
+
+            const productPromises = productsInOrder.map(
+              async (element: { _id: ObjectId; item: any }) => {
+                const product = await fastify
+                  .db(process.env.DB_NAME as string)
+                  ?.collection(CollectionEnum.Products)
+                  .findOne({ _id: new ObjectId(element._id) });
+
+                if (!product) {
+                  throw new Error(
+                    `Product with _id ${element.item._id} not found`
+                  );
+                }
+                if (product?.availableQuantity < element.item.quantity) {
+                  throw new Error(
+                    `Not enough quantity of product with _id ${element._id}`
+                  );
+                }
+
+                return product;
+              }
+            );
+
+            await Promise.all(productPromises);
+
+            // 3. update products quantity
+            await fastify
+              .db(process.env.DB_NAME as string)
+              ?.collection(CollectionEnum.Products)
+              .bulkWrite(
+                productsInOrder.map((el: { _id: ObjectId; item: any }) => ({
+                  updateOne: {
+                    filter: { _id: el._id },
+                    update: { $inc: { availableQuantity: -el.item.quantity } },
+                  },
+                })),
+                { session }
+              );
+
+            // 3. create order and order items one by one per store
+            await Promise.all(
+              ordersToStore.map(async (order) => {
+                const itms = productsInOrder
+                  .filter((item) => item.store === order.store)
+                  .map((item: any) => ({
+                    ...item.item,
+                    store: new ObjectId(item.store),
+                  }));
+
+                const orderItems = await fastify
+                  .db(process.env.DB_NAME as string)
+                  ?.collection(CollectionEnum.OrderItems)
+                  .insertMany(itms, { session });
+
+                const newOrder = await fastify
+                  .db(process.env.DB_NAME as string)
+                  ?.collection(CollectionEnum.Orders)
+                  .insertOne(
+                    {
+                      items: orderItems?.insertedIds
+                        ? Object.values(orderItems?.insertedIds)
+                        : [],
+                      customer,
+                      customerContact: {
+                        email: req.user.email,
+                        phoneNumber: userDetails?.phoneNumber,
+                      },
+                      store: new ObjectId(order.store),
+                      createdAt: new Date(),
+                      createdBy: customer,
+                      deliveryStatus: fastify.deliveryStatusEnum?.find(
+                        (status: { _id: ObjectId; deliveryStatus: string }) =>
+                          status?.deliveryStatus === DeliveryStatusEnum.ordered
+                      )._id,
+                    },
+                    { session }
+                  );
+
+                orderResult.push(newOrder);
+              })
+            );
+
+            //  await Promise.all(arr);
+
+            //5. TODO complete payment - if paymentType === card/ paypal
+            //6. TODO emit event
+            // in event handler -> TODO send confirmation email -> emit event
+            // in event handler -> create invoice
+          }
+        }, transactionOptions);
       } catch (error) {
         if (
           error instanceof MongoError &&
           error.hasErrorLabel('UnknownTransactionCommitResult')
         ) {
-          console.log(132, error);
+          console.error(JSON.stringify(error));
+          error;
         } else if (
           error instanceof MongoError &&
           error.hasErrorLabel('TransientTransactionError')
         ) {
-          console.log(137, error);
+          console.error(JSON.stringify(error));
         } else {
-          console.log(
-            'An error occurred in the transaction, performing a data rollback:' +
-              error
-          );
+          console.error(JSON.stringify(error));
         }
-        await session.abortTransaction();
       } finally {
         await session.endSession();
       }
-      console.log({ order });
-      //return order;
+      return orderResult;
     }
   );
 
--- src/features/products/routes/add.products.route.test.ts
+++ src/features/products/routes/add.products.route.test.ts
@@ -3,7 +3,7 @@ register();
 import { ObjectId } from 'mongodb';
 import { addProduct } from './add.product.route';
 
-describe('add Product route handler', function() {
+describe('add Product route handler', function () {
   let routeHandler: any;
   let mockFastify: any;
   let collection: any;
@@ -11,9 +11,9 @@ describe('add Product route handler', function() {
   let userId: any;
   let store: any;
 
-  beforeEach(function() {
+  beforeEach(function () {
     insertFn = jest.fn();
-    collection = jest.fn(() => ({ insertOne: insertFn, insertMany: insertFn }))
+    collection = jest.fn(() => ({ insertOne: insertFn, insertMany: insertFn }));
     mockFastify = {
       post: (_path: any, _opts: any, handler: never) => {
         routeHandler = handler;
@@ -29,15 +29,15 @@ describe('add Product route handler', function() {
 
     addProduct(mockFastify as never, undefined as never, () => null);
   });
-  it('works', function() {
+  it('works', function () {
     const body = [
       {
         name: 'testName',
         sku: 'testCode',
         productCategories: [new ObjectId()],
-        productTypes: new ObjectId()
+        productTypes: new ObjectId(),
       },
-    ]
+    ];
     routeHandler({
       body,
       user: { _id: userId, store },
@@ -45,56 +45,6 @@ describe('add Product route handler', function() {
 
     expect(mockFastify.db).toBeCalled();
     expect(collection).toBeCalled();
-    expect(insertFn.mock.calls[0][0]).toMatchObject(body)
+    expect(insertFn.mock.calls[0][0]).toMatchObject(body);
   });
 });
-
-// describe('delete Product route handler', function () {
-//   let routeHandler: any;
-//   let mockFastify: any;
-
-//   beforeEach(function () {
-//     mockFastify = {
-//       post: (_path: any, _opts: any, handler: never) => {
-//         routeHandler = handler;
-//       },
-//       db: jest.fn(),
-//     };
-
-//     deleteProduct(mockFastify as never, undefined as never, () => null);
-//   });
-
-//   it('works', function () {
-//     routeHandler({
-//       params: { id: '123' },
-//       user: { _id: 'test', store: 'test' },
-//     });
-
-//     expect(mockFastify.db).toBeCalledWith('marketplace');
-//   });
-// });
-
-// describe('delete Product route handler', function () {
-//   let routeHandler: any;
-//   let mockFastify: any;
-
-//   beforeEach(function () {
-//     mockFastify = {
-//       post: (_path: any, _opts: any, handler: never) => {
-//         routeHandler = handler;
-//       },
-//       db: jest.fn(),
-//     };
-
-//     deleteProduct(mockFastify as never, undefined as never, () => null);
-//   });
-
-//   it('works', function () {
-//     routeHandler({
-//       params: { id: '65665655' },
-//       user: { _id: 'someID', store: 'test' },
-//     });
-
-//     expect(mockFastify.db).toBeCalledWith('65665655');
-//   });
-// });
